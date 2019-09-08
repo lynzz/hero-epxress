471893236303b46128351892dd5eda0caa84b430
lynzz <lynzz168@gmail.com>
2019-09-08 23:13:06 +0800

    fix: update html

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const express = require('express')
 const path = require('path')
+const app        = express();
+const bodyParser = require('body-parser');
+const http       = require('http').Server(app);
+const io         = require('socket.io')(http);
 const PORT = process.env.PORT || 5000
 
-express()
+app.use(bodyParser.json({ type : '*/*' })); // force json
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.get('/', (req, res) => res.render('pages/index'))
+
+app.post('/locations', function(request, response){
+  console.log('Headers:\n', request.headers);
+  console.log('Locations:\n', request.body);
+  console.log('------------------------------');
+  io.emit('locations', request.body);
+  response.sendStatus(200);
+});
+
+app.post('/sync', function(request, response){
+  console.log('Headers:\n', request.headers);
+  console.log('Synced Locations:\n', request.body);
+  console.log('------------------------------');
+  io.emit('locations', request.body);
+  response.sendStatus(200);
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+
+http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
